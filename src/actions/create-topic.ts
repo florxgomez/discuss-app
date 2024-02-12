2a07b836d48aa1e7de0ae595705ea993ab3fac4a
Florencia Gomez <florxgomez@gmail.com>
2024-02-12 13:01:46 -0300

    add pages and server actions

---
+++ src/actions/create-topic.ts
@@ -0,0 +1,76 @@
+'use server';
+import { auth } from '@/auth';
+import { z } from 'zod';
+import type { Topic } from '@prisma/client';
+import { redirect } from 'next/navigation';
+import paths from '@/paths';
+import { db } from '@/db';
+import { revalidatePath } from 'next/cache';
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]/, {
+      message: 'Must be lowercase letters or dashes without spaces',
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const session = await auth();
+  const result = createTopicSchema.safeParse({
+    name: formData.get('name'),
+    description: formData.get('description'),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ['You must be signed in to do this action.'],
+      },
+    };
+  }
+  let topic: Topic;
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ['Something went wrong'],
+        },
+      };
+    }
+  }
+  revalidatePath('/');
+  redirect(paths.topicShow(topic.slug));
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Input,
+  Button,
+} from '@nextui-org/react';
+import Link from 'next/link';
+import HeaderAuth from './HeaderAuth';
+
+export default function Header() {
+  return (
+    <Navbar className="shadow mb-6">
+      <NavbarBrand>
+        <Link href="/" className="font-bold">
+          Discuss
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem>
+          <Input />
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <HeaderAuth />
+      </NavbarContent>
+    </Navbar>
+  );
+}
